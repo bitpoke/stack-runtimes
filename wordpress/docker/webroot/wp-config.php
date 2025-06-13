0bfc8a6b87db5d12d9a05b300decadf5f4ddbcd6
Mile Rosu <mile@bitpoke.io>
2025-06-13 10:46:48 +0300

    Add env vars compatibility with official https://hub.docker.com/_/wordpress image

--- wordpress/docker/webroot/wp-config.php
+++ wordpress/docker/webroot/wp-config.php
@@ -18,30 +18,51 @@
  * @package WordPress
  */
 
+// a helper function to lookup environment variables in a compatible manner with the official Docker image
+if ( ! function_exists( 'getenv_docker' ) ) {
+	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
+	function getenv_docker( $wordpress_env, $env, $default = null ) {
+		if ( null === $default ) {
+			$default = $env;
+			$env     = '';
+		}
+
+		if ( $fileEnv = getenv( $wordpress_env . '_FILE' ) ) {
+			return rtrim( file_get_contents( $fileEnv ), "\r\n" );
+		} elseif ( ( $val = getenv( $wordpress_env ) ) !== false ) {
+			return $val;
+		} elseif ( ! empty( $env ) && ( $val = getenv( $env ) ) !== false ) {
+			return $val;
+		} else {
+			return $default;
+		}
+	}
+}
+
 /**
  * Set up our global environment constant and load its config first
  * Default: production
  */
-define('WP_ENV', getenv('WP_ENV') ?: 'production');
+define( 'WP_ENV', getenv( 'WP_ENV' ) ?: 'production' );
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') ?: 'wordpress' );
+define( 'DB_NAME', getenv_docker( 'WORDPRESS_DB_NAME', 'DB_NAME', 'wordpress' ) );
 
 /** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') ?: '' );
+define( 'DB_USER', getenv_docker( 'WORDPRESS_DB_USER', 'DB_USER', 'root' ) );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') ?: '' );
+define( 'DB_PASSWORD', getenv_docker( 'WORDPRESS_DB_PASSWORD', 'DB_PASSWORD', '' ) );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') ?: 'localhost' );
+define( 'DB_HOST', getenv_docker( 'WORDPRESS_DB_HOST', 'DB_HOST', 'localhost' ) );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', getenv( 'DB_CHARSET' ) ?: 'utf8' );
+define( 'DB_CHARSET', getenv_docker( 'WORDPRESS_DB_CHARSET', 'DB_CHARSET', 'utf8' ) );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) ?: '' );
+define( 'DB_COLLATE', getenv_docker( 'WORDPRESS_DB_COLLATE', 'DB_COLLATE', '' ) );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -52,14 +73,14 @@ define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) ?: '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv( 'AUTH_KEY' ) ?: 'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  getenv( 'SECURE_AUTH_KEY' ) ?: 'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    getenv( 'LOGGED_IN_KEY' ) ?: 'put your unique phrase here' );
-define( 'NONCE_KEY',        getenv( 'NONCE_KEY' ) ?: 'put your unique phrase here' );
-define( 'AUTH_SALT',        getenv( 'AUTH_SALT' ) ?: 'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', getenv( 'SECURE_AUTH_SALT' ) ?: 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   getenv( 'LOGGED_IN_SALT' ) ?: 'put your unique phrase here' );
-define( 'NONCE_SALT',       getenv( 'NONCE_SALT' ) ?: 'put your unique phrase here' );
+define( 'AUTH_KEY', getenv_docker( 'WORDPRESS_AUTH_KEY', 'AUTH_KEY', 'put your unique phrase here' ) );
+define( 'SECURE_AUTH_KEY', getenv_docker( 'WORDPRESS_SECURE_AUTH_KEY', 'SECURE_AUTH_KEY', 'put your unique phrase here' ) );
+define( 'LOGGED_IN_KEY', getenv_docker( 'WORDPRESS_LOGGED_IN_KEY', 'LOGGED_IN_KEY', 'put your unique phrase here' ) );
+define( 'NONCE_KEY', getenv_docker( 'WORDPRESS_NONCE_KEY', 'NONCE_KEY', 'put your unique phrase here' ) );
+define( 'AUTH_SALT', getenv_docker( 'WORDPRESS_AUTH_SALT', 'AUTH_SALT', 'put your unique phrase here' ) );
+define( 'SECURE_AUTH_SALT', getenv_docker( 'WORDPRESS_SECURE_AUTH_SALT', 'SECURE_AUTH_SALT', 'put your unique phrase here' ) );
+define( 'LOGGED_IN_SALT', getenv_docker( 'WORDPRESS_LOGGED_IN_SALT', 'LOGGED_IN_SALT', 'put your unique phrase here' ) );
+define( 'NONCE_SALT', getenv_docker( 'WORDPRESS_NONCE_SALT', 'NONCE_SALT', 'put your unique phrase here' ) );
 
 /**#@-*/
 
@@ -69,43 +90,65 @@ define( 'NONCE_SALT',       getenv( 'NONCE_SALT' ) ?: 'put your unique phrase he
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = getenv('DB_PREFIX') ?: 'wp_';
+$table_prefix = getenv_docker( 'WORDPRESS_TABLE_PREFIX', 'DB_PREFIX', 'wp_' );
 
 /**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
+ *
+ * we include this by default because reverse proxying is extremely common in container environments
  */
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
+	$_SERVER['HTTPS'] = 'on';
 }
 
 
 if ( defined( 'WP_CLI' ) && WP_CLI && ! isset( $_SERVER['HTTP_HOST'] ) ) {
-    $_SERVER['HTTP_HOST'] = parse_url( getenv( 'WP_HOME' ) ?: getenv( 'WP_SITEURL' ), PHP_URL_HOST );
+	$_SERVER['HTTP_HOST'] = parse_url( getenv( 'WP_HOME' ) ?: getenv( 'WP_SITEURL' ), PHP_URL_HOST );
+}
+
+if ( $configExtra = getenv_docker( 'WORDPRESS_CONFIG_EXTRA', '' ) ) {
+	eval( $configExtra );
 }
 
 $user_config = dirname( __DIR__ ) . '/config/wp-config.php';
 if ( file_exists( $user_config ) ) {
-    require_once $user_config;
+	require_once $user_config;
+}
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+if ( ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', (bool) getenv_docker( 'WORDPRESS_DEBUG', '' ) );
 }
 
 if ( defined( 'WP_CLI' ) && WP_CLI && ! isset( $_SERVER['HTTP_HOST'] ) ) {
 	$_wp_cli_detected_host = parse_url( defined( 'WP_HOME' ) ? WP_HOME : '', PHP_URL_HOST );
 
-	if ( empty( $_wp_cli_detected_host	) ) {
+	if ( empty( $_wp_cli_detected_host ) ) {
 		$_wp_cli_detected_host = parse_url( defined( 'WP_SITEURL' ) ? WP_SITEURL : '', PHP_URL_HOST );
 	}
 
-	if ( empty( $_wp_cli_detected_host	) ) {
+	if ( empty( $_wp_cli_detected_host ) ) {
 		$_wp_cli_detected_host = parse_url( getenv( 'WP_HOME' ) ?: getenv( 'WP_SITEURL' ), PHP_URL_HOST );
 	}
 
-	if ( empty( $_wp_cli_detected_host	) ) {
+	if ( empty( $_wp_cli_detected_host ) ) {
 		trigger_error( 'Unable to detect HTTP_HOST for WP-CLI. You must either set WP_HOME and WP_SITEURL environment variables of use --url argument when calling WP-CLI.', E_USER_ERROR );
-		exit(1);
+		exit( 1 );
 	}
 
-    $_SERVER['HTTP_HOST'] = $_wp_cli_detected_host;
+	$_SERVER['HTTP_HOST'] = $_wp_cli_detected_host;
 	unset( $_wp_cli_detected_host );
 }
 
@@ -144,19 +187,19 @@ if ( ! defined( 'DISABLE_WP_CRON' ) ) {
 }
 if ( ! defined( 'DISALLOW_FILE_EDIT' ) ) {
 	// Disable the plugin and theme file editor in the admin
-	define( 'DISALLOW_FILE_EDIT', "true" === strtolower( getenv( 'DISALLOW_FILE_EDIT' ) ?: "true" ) );
+	define( 'DISALLOW_FILE_EDIT', 'true' === strtolower( getenv( 'DISALLOW_FILE_EDIT' ) ?: 'true' ) );
 }
 if ( ! defined( 'DISALLOW_FILE_MODS' ) ) {
 	// Disable plugin and theme updates and installation from the admin
-	define( 'DISALLOW_FILE_MODS', "true" === strtolower( getenv( 'DISALLOW_FILE_MODS' ) ?: "true" ) );
+	define( 'DISALLOW_FILE_MODS', 'true' === strtolower( getenv( 'DISALLOW_FILE_MODS' ) ?: 'true' ) );
 }
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+	define( 'ABSPATH', __DIR__ . '/wp/' );
 }
 
 /** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
+require_once ABSPATH . 'wp-settings.php';
